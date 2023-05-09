8f04693e8e32ae0ffef7971590a5a43964d46800
mariaa <127808938+mariaoktadian@users.noreply.github.com>
2023-05-09 12:13:21 +0800

    Add files via upload

---
+++ SESI9/javascript01.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan JavaScript</title>
+</head>
+<body>
+    
+    <script>
+        document.write("<center>Belajar Script JavaScript <br></center>");
+        document.write("Belajar Script JavaScript");
+
+        var bil1 = 5;
+        var bil2 = 7;
+        var hasil = bil1 + bil2;
+        document.write("<br>Hasil Penjumlahan dari "+bil1+" dengan "+bil2+" adalah "+hasil);
+        
+        for(let i=0; i<10; i++){
+            document.write("<br>Perulangan ke-"+i);
+        }
+    </script>
+</body>
+</html>
---
+++ SESI9/javascript04.js
@@ -0,0 +1,10 @@
+function tulis(){
+    document.write("contoh text dengan menggunakan JavaScript");
+    document.write("<br> Text Tambahan");
+}
+
+function checknama(){
+    let nm = document.getElementById("txNAMA").value;
+
+    console.log("isi dari nm"+ nm);
+}
